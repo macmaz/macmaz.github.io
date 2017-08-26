ddb9448faab4d034566085419f6a8551fe8f72be
macmaz <maciej.mazurek87@gmail.com>
2017-08-26 19:26:32 +0200

    delatad json array

--- zadania/1-pobierz-dane-programisty/js/script.js
+++ zadania/1-pobierz-dane-programisty/js/script.js
@@ -1,13 +1,6 @@
 
 
 $("#btn1").click(function(){
-    
-var dane = [{
-   "imie": "Piotr",
-   "zawod": "Programista",
-   "nazwisko": "Lewinski",
-   "firma": "Akademia108"
-}]
 
 $.getJSON("http://echo.jsontest.com/imie/Piotr/nazwisko/Lewinski/zawod/Programista/firma/Akademia108", function(wynik){
 $.each(wynik, function(index, wartosc){
