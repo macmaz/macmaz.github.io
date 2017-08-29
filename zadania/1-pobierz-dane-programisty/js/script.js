78af4848ffc0825ee23e991c3b7fd873d7cbbeb4
macmaz <maciej.mazurek87@gmail.com>
2017-08-29 20:10:50 +0200

    new link

--- zadania/1-pobierz-dane-programisty/js/script.js
+++ zadania/1-pobierz-dane-programisty/js/script.js
@@ -1,11 +1,10 @@
 $("#btn1").click(function(){
     
-$.getJSON("https://akademia108.pl/kurs-front-end/ajax/1-pobierz-dane-programisty.php", { function(wynik){
+$.getJSON("https://akademia108.pl/kurs-front-end/ajax/1-pobierz-dane-programisty.php", function(wynik){
         
 $.each(wynik, function(index, wartosc){
     
 $("#dane-programisty").append(index + ": " + wartosc + "\n");
         });
-}
 });
 });
