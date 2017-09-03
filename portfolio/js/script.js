0ce82977e1a675b38a7640705281f5955bb6b029
macmaz <maciej.mazurek87@gmail.com>
2017-09-03 22:54:12 +0200

    jquery smooth scroll

--- portfolio/js/script.js
+++ portfolio/js/script.js
@@ -23,3 +23,13 @@ function navNewClass() {
          $('a.navLink').removeClass('scroll');
     }
 }
+
+
+$('.navLink').click(function () {
+    var link = $(this).attr("href");
+    var element = link.substr(link.indexOf("#"));
+    $('html, body').animate({
+        scrollTop: $(element).offset().top
+    }, 1000);
+    return false;
+});
